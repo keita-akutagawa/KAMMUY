0389857adaaf19e1db9f54f1275952c672c85a03
Keita-A <keita19990517@gmail.com>
2024-07-29 18:21:04 +0900

    20240729

--- Interface2D/const.hpp
+++ Interface2D/const.hpp
@@ -3,10 +3,6 @@ namespace Interface2DConst
 {
     extern const float PI;
 
-    extern const int interfaceLength;
-    extern const int indexOfInterfaceStartInMHD;
-    extern const int indexOfInterfaceStartInPIC;
-
     extern const int windowSizeForRemoveNoiseByConvolution;
 
     extern const unsigned long long reloadParticlesTotalNumIon;
@@ -14,10 +10,6 @@ namespace Interface2DConst
 
 
     extern __constant__ float device_PI;
-
-    extern __constant__ int device_interfaceLength;
-    extern __constant__ int device_indexOfInterfaceStartInMHD;
-    extern __constant__ int device_indexOfInterfaceStartInPIC;
 
     extern __constant__ int device_windowSizeForRemoveNoiseByConvolution;
 
