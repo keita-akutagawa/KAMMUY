ca52c619dbdbd137b56eaa4a70ec5d167d9abd03
Keita <keita19990517@gmail.com>
2024-08-13 17:01:20 +0900

    20240813

--- examples2D/alfven/main_alfven_const.hpp
+++ examples2D/alfven/main_alfven_const.hpp
@@ -26,7 +26,7 @@ std::string directoryname = "results_alfven";
 std::string filenameWithoutStep = "alfven";
 std::ofstream logfile("results_alfven/log_alfven.txt");
 
-const int IdealMHD2DConst::totalStep_MHD = 5;
+const int IdealMHD2DConst::totalStep_MHD = 10;
 const int PIC2DConst::totalStep_PIC = -1;
 const int recordStep = 1;
 const bool isParticleRecord = false;
