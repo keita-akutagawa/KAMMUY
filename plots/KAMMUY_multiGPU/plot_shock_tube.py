33fc09a9e19cbf32c5db4a324b9553ca71c3be9b
Keita-A <keita19990517@gmail.com>
2024-12-20 16:55:48 +0900

    20241220

--- plots/KAMMUY_multiGPU/plot_shock_tube.py
+++ plots/KAMMUY_multiGPU/plot_shock_tube.py
@@ -1,7 +1,5 @@
 import numpy as np
-from scipy import stats
 import matplotlib.pyplot as plt
-import cv2
 
 
 # PIC
@@ -12,11 +10,11 @@ m_electron = 1.0
 r_m = 1 / 25
 m_ion = m_electron / r_m
 t_r = 1.0
-ne0_pic = 50 #ここは手動で調整すること
+ne0_pic = 200
 ni0_pic = ne0_pic
 B0_pic = np.sqrt(ne0_pic) / 1.0
-Ti_pic  = (B0_pic**2 / 2.0 / mu_0) / (ni0_pic + ne0_pic * t_r)
-Te_pic = Ti_pic * t_r
+Te_pic = 0.5 * m_electron * (np.sqrt(0.02)*c)**2
+Ti_pic = Te_pic / t_r
 q_electron = -1 * np.sqrt(epsilon0 * Te_pic / ne0_pic)
 q_ion = -1 * q_electron
 debye_length = np.sqrt(epsilon0 * Te_pic / ne0_pic / q_electron**2)
@@ -32,19 +30,23 @@ CS_pic = np.sqrt(gamma_pic * p_pic / rho_pic)
 v_thermal_electron = np.sqrt(2.0 * Te_pic / m_electron)
 v_thermal_ion = np.sqrt(2.0 * Ti_pic / m_ion)
 ion_inertial_length = c / omega_pi
-electron_inertial_length = c / omega_pe
-sheat_thickness = 1.0 * ion_inertial_length
 
 dx_pic = 1.0
-nx_pic = 20000
-x_min_pic = 1e-10
-x_max_pic = nx_pic * dx_pic - 1e-10
+nx_pic = 20
+x_min_pic = 0.0
+x_max_pic = nx_pic * dx_pic
 x_coordinate_pic = np.arange(0.0, x_max_pic, dx_pic)
 dy_pic = 1.0
-ny_pic = 100
-y_min_pic = 1e-10
-y_max_pic = ny_pic * dy_pic - 1e-10
+ny_pic = 400
+y_min_pic = 0.0
+y_max_pic = ny_pic * dy_pic
 y_coordinate_pic = np.arange(0.0, y_max_pic, dy_pic)
+
+X_pic, Y_pic = np.meshgrid(x_coordinate_pic, y_coordinate_pic)
+
+n_ion = int(ni0_pic * nx_pic)
+n_electron = int(n_ion * abs(q_ion / q_electron))
+
 
 # MHD
 gamma_mhd = 5.0 / 3.0
@@ -61,60 +63,75 @@ nx_mhd = nx_pic
 x_min_mhd = 0.0
 x_max_mhd = nx_mhd * dx_mhd
 dy_mhd = dy_pic
-ny_mhd = 2000
+ny_mhd = 350
 y_min_mhd = 0.0
 y_max_mhd = ny_mhd * dy_mhd
+
 x_coordinate_mhd = np.arange(x_min_mhd, x_max_mhd, dx_mhd)
 y_coordinate_mhd = np.arange(y_min_mhd, y_max_mhd, dy_mhd)
+X_mhd, Y_mhd = np.meshgrid(x_coordinate_mhd, y_coordinate_mhd)
 
-# Interface
-window_size = 5
-interface_width = 20
+interface_width = 50
 
 
 # load data
-procs = 8
+procs = 1
 buffer = 3
-dirname = ""
-step = 1200
+dirname = "/cfca-work/akutagawakt/KAMMUY_multiGPU/results_shock_tube"
+step = 1000
 savename = f"{step}.png"
 
-total_field_pic = np.zeros([nx_pic, ny_pic])
-total_field_lower_mhd = np.zeros([nx_mhd, ny_mhd])
-total_field_upper_mhd = np.zeros([nx_mhd, ny_mhd])
+total_field_pic = np.zeros([nx_pic + 2 * buffer, ny_pic + 2 * buffer])
+total_field_mhd = np.zeros([nx_mhd + 2 * buffer, ny_mhd + 2 * buffer])
 for rank in range(procs):
     local_grid_x = rank 
     local_grid_y = 0
-    local_nx_pic = nx_pic // procs
-    local_ny_pic = ny_pic // 1
-    local_nx_mhd = nx_mhd // procs
-    local_ny_mhd = ny_mhd // 1
+    local_nx_pic = int(nx_pic // procs + 2 * buffer)
+    local_ny_pic = int(ny_pic // 1 + 2 * buffer)
+    local_nx_mhd = int(nx_mhd // procs + 2 * buffer)
+    local_ny_mhd = int(ny_mhd // 1 + 2 * buffer)
 
-    filename = f"{dirname}/current_sheet_large_B_{step}_{rank}.bin"
+    filename = f"{dirname}/shock_tube_B_{step}_{rank}.bin"
     with open(filename, 'rb') as f:
         B = np.fromfile(f, dtype=np.float32)
     B = B.reshape(local_nx_pic, local_ny_pic, 3).T
-    filename = f"{dirname}/current_sheet_large_E_{step}_{rank}.bin"
+    filename = f"{dirname}/shock_tube_E_{step}_{rank}.bin"
     with open(filename, 'rb') as f:
         E = np.fromfile(f, dtype=np.float32)
     E = E.reshape(local_nx_pic, local_ny_pic, 3).T
-    filename = f"{dirname}/current_sheet_large_current_{step}_{rank}.bin"
+    filename = f"{dirname}/shock_tube_current_{step}_{rank}.bin"
     with open(filename, 'rb') as f:
         current = np.fromfile(f, dtype=np.float32)
     current = current.reshape(local_nx_pic, local_ny_pic, 3).T
 
-    filename = f"{dirname}/current_sheet_large_zeroth_moment_ion_{step}_{rank}.bin"
+    filename = f"{dirname}/shock_tube_zeroth_moment_ion_{step}_{rank}.bin"
     with open(filename, 'rb') as f:
         zeroth_moment_ion = np.fromfile(f, dtype=np.float32)
     zeroth_moment_ion = zeroth_moment_ion.reshape(local_nx_pic, local_ny_pic).T
-    filename = f"{dirname}/current_sheet_large_zeroth_moment_electron_{step}_{rank}.bin"
+    filename = f"{dirname}/shock_tube_zeroth_moment_electron_{step}_{rank}.bin"
     with open(filename, 'rb') as f:
         zeroth_moment_electron = np.fromfile(f, dtype=np.float32)
     zeroth_moment_electron = zeroth_moment_electron.reshape(local_nx_pic, local_ny_pic).T
+    filename = f"{dirname}/shock_tube_first_moment_ion_{step}_{rank}.bin"
+    with open(filename, 'rb') as f:
+        first_moment_ion = np.fromfile(f, dtype=np.float32)
+    first_moment_ion = first_moment_ion.reshape(local_nx_pic, local_ny_pic, 3).T
+    filename = f"{dirname}/shock_tube_first_moment_electron_{step}_{rank}.bin"
+    with open(filename, 'rb') as f:
+        first_moment_electron = np.fromfile(f, dtype=np.float32)
+    first_moment_electron = first_moment_electron.reshape(local_nx_pic, local_ny_pic, 3).T
+    filename = f"{dirname}/shock_tube_second_moment_ion_{step}_{rank}.bin"
+    with open(filename, 'rb') as f:
+        second_moment_ion = np.fromfile(f, dtype=np.float32)
+    second_moment_ion = second_moment_ion.reshape(local_nx_pic, local_ny_pic, 6).T
+    filename = f"{dirname}/shock_tube_second_moment_electron_{step}_{rank}.bin"
+    with open(filename, 'rb') as f:
+        second_moment_electron = np.fromfile(f, dtype=np.float32)
+    second_moment_electron = second_moment_electron.reshape(local_nx_pic, local_ny_pic, 6).T
 
     rho_pic = m_ion * zeroth_moment_ion + m_electron * zeroth_moment_electron
 
-    filename = f"{dirname}/current_sheet_large_U_lower_{step}_{rank}.bin"
+    filename = f"{dirname}/shock_tube_U_lower_{step}_{rank}.bin"
     with open(filename, 'rb') as f:
         U_lower = np.fromfile(f, dtype=np.float64)
     U_lower = U_lower.reshape(local_nx_mhd, local_ny_mhd, 8).T
@@ -140,7 +157,7 @@ for rank in range(procs):
     current_lower_z_mhd[0] = current_lower_z_mhd[1] 
     current_lower_z_mhd[-1] = current_lower_z_mhd[-2] 
 
-    filename = f"{dirname}/current_sheet_large_U_upper_{step}_{rank}.bin"
+    filename = f"{dirname}/shock_tube_U_upper_{step}_{rank}.bin"
     with open(filename, 'rb') as f:
         U_upper = np.fromfile(f, dtype=np.float64)
     U_upper = U_upper.reshape(local_nx_mhd, local_ny_mhd, 8).T
@@ -166,50 +183,36 @@ for rank in range(procs):
     current_upper_z_mhd[0] = current_upper_z_mhd[1] 
     current_upper_z_mhd[-1] = current_upper_z_mhd[-2] 
 
-    total_field_lower_mhd[
+    total_field_mhd[
         int(local_grid_x * local_nx_mhd) : int((local_grid_x + 1) * local_nx_mhd), 
         int(local_grid_y * local_ny_mhd) : int((local_grid_y + 1) * local_ny_mhd)
-    ] = rho_lower_mhd.T
-    total_field_upper_mhd[
-        int(local_grid_x * local_nx_mhd) : int((local_grid_x + 1) * local_nx_mhd), 
-        int(local_grid_y * local_ny_mhd) : int((local_grid_y + 1) * local_ny_mhd)
-    ] = rho_upper_mhd.T
+    ] = Bx_lower_mhd.T
     total_field_pic[
         int(local_grid_x * local_nx_pic) : int((local_grid_x + 1) * local_nx_pic), 
         int(local_grid_y * local_ny_pic) : int((local_grid_y + 1) * local_ny_pic)
-    ] = rho_pic.T
+    ] = B[2].T
 
 
-fig = plt.figure(figsize=(12, 6), constrained_layout=True)
-ax1 = fig.add_subplot(211)
-ax2 = fig.add_subplot(212)
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111)
 
-X_pic, Y_pic = np.meshgrid(x_coordinate_pic, y_coordinate_pic - 0.5 * y_max_pic) / ion_inertial_length
-mappable = ax1.pcolormesh(X_pic, Y_pic, total_field_pic.T / rho0_mhd, cmap='jet', vmin=0.0, vmax=2.0)
+ax.plot(total_field_pic[10, :])
+#ax.plot(total_field_mhd[15, :])
+#ax.set_xlim(0, 1000)
+#mappable = ax.pcolormesh(total_field_pic.T, cmap='jet')
+#cbar = fig.colorbar(mappable, ax=ax)
+#ax.set_ylim(ny_mhd - 50, ny_mhd + 6)
+#cbar.set_label(r'$  $', fontsize=24, rotation=90, labelpad=10)
+#cbar.ax.tick_params(labelsize=20)
 
-X_mhd, Y_mhd = np.meshgrid(x_coordinate_mhd, y_coordinate_mhd) / ion_inertial_length
-ax2.pcolormesh(X_mhd, Y_mhd - (ny_mhd + 0.5 * ny_pic - interface_width) / ion_inertial_length, total_field_lower_mhd.T / rho0_mhd, cmap='jet', vmin=0.0, vmax=2.0)
-ax2.pcolormesh(X_mhd, Y_mhd + (ny_pic - 0.5 * ny_pic - interface_width) / ion_inertial_length, total_field_upper_mhd.T / rho0_mhd, cmap='jet', vmin=0.0, vmax=2.0)
+#ax.text(0.5, 1.05, f"{step / (1.0 / omega_ci / dt):.2f}" + r" $\Omega_{{ci}}^{{-1}}$", ha='center', transform=ax.transAxes, fontsize=32)
+#ax.set_xlabel(r'$x / \lambda_i$', fontsize=20)
+#ax.set_ylabel(r'$y / \lambda_i$', fontsize=20)
+#ax.set_xlim(30, 70)
+#ax.set_ylim(0, 100)
+ax.tick_params(labelsize=18)
 
-cbar = fig.colorbar(mappable, ax=[ax1, ax2], pad=0.05, shrink=0.7, aspect=10, ticks=np.arange(0.0, 2.1, 0.5))
-cbar.set_label(r'$\rho / \rho_0$', fontsize=24)
-cbar.ax.tick_params(labelsize=18)
-
-ax1.set_xlim(0, nx_pic / ion_inertial_length)
-ax1.set_ylim(-ny_mhd / ion_inertial_length, ny_mhd / ion_inertial_length)
-ax2.set_xlim(0, nx_pic / ion_inertial_length)
-ax2.set_ylim(-ny_mhd / ion_inertial_length, ny_mhd / ion_inertial_length)
-ax1.set_aspect("equal")
-ax2.set_aspect("equal")
-
-ax1.set_xlabel(r'$x / \lambda_i$', fontsize=20)
-ax1.set_ylabel(r'$y / \lambda_i$', fontsize=20)
-ax1.tick_params(labelsize=18)
-
-ax2.set_xlabel(r'$x / \lambda_i$', fontsize=20)
-ax2.set_ylabel(r'$y / \lambda_i$', fontsize=20)
-ax2.tick_params(labelsize=18)
-
+plt.tight_layout()
 fig.savefig(savename, dpi=200)
 
 
